fccbc038f03afe0dd43322c1e521b0a1ebbbda2e
youtehyuk <youtehyuk0126@gmail.com>
2025-09-30 07:09:03 +0000

    new printf added -hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,10 @@
-printf("Hello Worlds");
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	printf("Hello World Again!\n");
+	printf("Hello World in testing branch!\n");
+
+	return 0;
+}
